c594e0c6b876545ee4cc68b45d4f4cd8cef2a553
Assaf Basri <Faskoman90@gmail.com>
2023-11-29 19:04:06 +0200

    style(__button): fixed no-project-container

--- app.ts
+++ app.ts
@@ -1,7 +1,14 @@
-import { loadPage } from "./quicklist.controller.js";
+import { loadPage } from "./quicklist.controller.js"
 // import { users } from "./login.js";
 
 // const retrieveUsers = JSON.parse(localStorage.getItem("users")!);
 // users = sessionStorage.getItem(retrieveUsers);
 
-loadPage();
+document
+  .getElementById("logoutButton")
+  ?.addEventListener("click", function (event) {
+    event.preventDefault()
+    window.location.href = "login.html"
+  })
+
+loadPage()
