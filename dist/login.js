489968258f586854540a755b311df3b083fb8452
Assaf Basri <Faskoman90@gmail.com>
2023-11-29 22:52:57 +0200

    fix(login): works now

--- dist/login.js
+++ dist/login.js
@@ -4,57 +4,48 @@ if (retrieveUsers) {
     users = JSON.parse(retrieveUsers);
 }
 const loginForm = document.querySelector("form[name='login']");
-if (!loginForm) {
-    console.error("Couldn't find login form.");
-}
-else {
-    loginForm.addEventListener("submit", function (e) {
-        const formData = new FormData(e.target);
-        const username = getString(formData, "username");
-        const password = getString(formData, "password");
-        if (username && password) {
-            try {
-                login(username, password);
-            }
-            catch (error) {
-                console.error("no username or password");
-            }
+loginForm?.addEventListener("submit", function (e) {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const username = getString(formData, "login-username");
+    const password = getString(formData, "login-password");
+    if (username && password) {
+        try {
+            login(username, password);
         }
-    });
-}
+        catch (error) {
+            console.error("no username or password");
+        }
+    }
+});
 const addUserForm = document.querySelector("form[name='sign-up']");
-if (!addUserForm) {
-    console.error("Couldn't find add user form.");
-}
-else {
-    addUserForm.addEventListener("submit", function (e) {
-        e.preventDefault();
-        const formData = new FormData(e.target);
-        const username = getRequiredString(formData, "username");
-        const email = getRequiredString(formData, "email");
-        if (users.some((user) => user.username === username)) {
-            throw new Error(`Username ${username} already taken`);
-        }
-        else if (users.some((user) => user.email === email)) {
-            throw new Error(`email ${email} already taken`);
-        }
-        else {
-            users.push({
-                fullname: getRequiredString(formData, "fullname"),
-                username: getRequiredString(formData, "username"),
-                password: getRequiredString(formData, "password"),
-                email: getRequiredString(formData, "e-mail"),
-                level: 1,
-                xp: 0,
-                about: "",
-                avatar: "1",
-                myProjects: [],
-            });
-            window.localStorage.setItem("users", JSON.stringify(users));
-            window.location.href = "index.html";
-        }
-    });
-}
+addUserForm?.addEventListener("submit", function (e) {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const username = getRequiredString(formData, "username");
+    const email = getRequiredString(formData, "email");
+    if (users.some((user) => user.username === username)) {
+        throw new Error(`Username ${username} already taken`);
+    }
+    else if (users.some((user) => user.email === email)) {
+        throw new Error(`email ${email} already taken`);
+    }
+    else {
+        users.push({
+            fullname: getRequiredString(formData, "fullname"),
+            username: getRequiredString(formData, "username"),
+            password: getRequiredString(formData, "password"),
+            email: getRequiredString(formData, "email"),
+            level: 1,
+            xp: 0,
+            about: "",
+            avatar: "1",
+            myProjects: [],
+        });
+        window.localStorage.setItem("users", JSON.stringify(users));
+        window.location.href = "index.html";
+    }
+});
 function getString(formData, key) {
     const value = formData.get(key);
     if (value == null) {
@@ -83,17 +74,3 @@ function login(username, password) {
     sessionStorage.setItem("user", user.username);
     window.location.href = "index.html";
 }
-// document.addEventListener("`DOMContentLoaded" ,() => {
-//   const signupForm = document.querySelector("#formSignin");
-//   const loginForm = document.querySelector("#formLogin");
-//   document.querySelector("#linkSignup")?.addEventListener("click", (e) => {
-//     e.preventDefault();
-//     loginForm?.classList.add("form__hidden");
-//     signupForm?.classList.remove("form__hidden");
-//   });
-//   document.querySelector("#linkLogin")?.addEventListener("click", (e) => {
-//     e.preventDefault();
-//     loginForm?.classList.remove("form__hidden");
-//     signupForm?.classList.add("form__hidden");
-//   })
-// });
--- login.ts
+++ login.ts
@@ -20,60 +20,55 @@ if (retrieveUsers) {
 const loginForm = document.querySelector(
   "form[name='login']",
 ) as HTMLFormElement | null
-if (!loginForm) {
-  console.error("Couldn't find login form.")
-} else {
-  loginForm.addEventListener("submit", function (e) {
-    const formData = new FormData(e.target as HTMLFormElement)
 
-    const username = getString(formData, "username")
-    const password = getString(formData, "password")
+loginForm?.addEventListener("submit", function (e) {
+  e.preventDefault()
 
-    if (username && password) {
-      try {
-        login(username, password)
-      } catch (error) {
-        console.error("no username or password")
-      }
+  const formData = new FormData(e.target as HTMLFormElement)
+
+  const username = getString(formData, "login-username")
+  const password = getString(formData, "login-password")
+
+  if (username && password) {
+    try {
+      login(username, password)
+    } catch (error) {
+      console.error("no username or password")
     }
-  })
-}
+  }
+})
 
 const addUserForm = document.querySelector(
   "form[name='sign-up']",
 ) as HTMLFormElement | null
-if (!addUserForm) {
-  console.error("Couldn't find add user form.")
-} else {
-  addUserForm.addEventListener("submit", function (e) {
-    e.preventDefault()
+addUserForm?.addEventListener("submit", function (e) {
+  e.preventDefault()
 
-    const formData = new FormData(e.target as HTMLFormElement)
-    const username = getRequiredString(formData, "username")
-    const email = getRequiredString(formData, "email")
+  const formData = new FormData(e.target as HTMLFormElement)
+  const username = getRequiredString(formData, "username")
+  const email = getRequiredString(formData, "email")
 
-    if (users.some((user) => user.username === username)) {
-      throw new Error(`Username ${username} already taken`)
-    } else if (users.some((user) => user.email === email)) {
-      throw new Error(`email ${email} already taken`)
-    } else {
-      users.push({
-        fullname: getRequiredString(formData, "fullname"),
-        username: getRequiredString(formData, "username"),
-        password: getRequiredString(formData, "password"),
-        email: getRequiredString(formData, "e-mail"),
-        level: 1,
-        xp: 0,
-        about: "",
-        avatar: "1",
-        myProjects: [],
-      })
+  if (users.some((user) => user.username === username)) {
+    throw new Error(`Username ${username} already taken`)
+  } else if (users.some((user) => user.email === email)) {
+    throw new Error(`email ${email} already taken`)
+  } else {
+    users.push({
+      fullname: getRequiredString(formData, "fullname"),
+      username: getRequiredString(formData, "username"),
+      password: getRequiredString(formData, "password"),
+      email: getRequiredString(formData, "email"),
+      level: 1,
+      xp: 0,
+      about: "",
+      avatar: "1",
+      myProjects: [],
+    })
 
-      window.localStorage.setItem("users", JSON.stringify(users))
-      window.location.href = "index.html"
-    }
-  })
-}
+    window.localStorage.setItem("users", JSON.stringify(users))
+    window.location.href = "index.html"
+  }
+})
 
 function getString(formData: FormData, key: string) {
   const value = formData.get(key)
@@ -91,6 +86,7 @@ function getString(formData: FormData, key: string) {
   }
   return value
 }
+
 function getRequiredString(formData: FormData, key: string) {
   const value = getString(formData, key)
 
@@ -100,6 +96,7 @@ function getRequiredString(formData: FormData, key: string) {
 
   return value
 }
+
 function login(username: string, password: string) {
   const user = users.find(
     (user) => user.username === username && user.password === password,
@@ -112,21 +109,3 @@ function login(username: string, password: string) {
   sessionStorage.setItem("user", user.username)
   window.location.href = "index.html"
 }
-
-// document.addEventListener("`DOMContentLoaded" ,() => {
-//   const signupForm = document.querySelector("#formSignin");
-//   const loginForm = document.querySelector("#formLogin");
-
-//   document.querySelector("#linkSignup")?.addEventListener("click", (e) => {
-//     e.preventDefault();
-//     loginForm?.classList.add("form__hidden");
-//     signupForm?.classList.remove("form__hidden");
-//   });
-
-//   document.querySelector("#linkLogin")?.addEventListener("click", (e) => {
-//     e.preventDefault();
-//     loginForm?.classList.remove("form__hidden");
-//     signupForm?.classList.add("form__hidden");
-//   })
-
-// });
