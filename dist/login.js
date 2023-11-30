b4a55421a6ebdcac37c8c76a2cdd1ccae7794f73
Assaf Basri <Faskoman90@gmail.com>
2023-11-30 18:09:49 +0200

    feat(inject): dashboard

--- dist/login.js
+++ dist/login.js
@@ -1,65 +1,9 @@
-let levels = [
-    {
-        Name: "The New Guy",
-        number: 1,
-        Description: "Just getting started, still learning the ropes.",
-        LevelUnlockScore: -Infinity,
-    },
-    {
-        Name: "The Apprentice",
-        number: 2,
-        Description: "Making progress, gaining experience.",
-        LevelUnlockScore: 10,
-    },
-    {
-        Name: "The Junior Achiever",
-        number: 3,
-        Description: "Competent and consistently delivering good work.",
-        LevelUnlockScore: 25,
-    },
-    {
-        Name: "The Efficient Contributor",
-        number: 4,
-        Description: "Demonstrating efficiency and reliability.",
-        LevelUnlockScore: 50,
-    },
-    {
-        Name: "The Skilled Performer",
-        number: 5,
-        Description: "Highly skilled, consistently delivering quality work.",
-        LevelUnlockScore: 100,
-    },
-    {
-        Name: "The Senior Contributor",
-        number: 6,
-        Description: " Highly skilled, consistently delivering quality work.",
-        LevelUnlockScore: 250,
-    },
-    {
-        Name: "The Expert Achiever",
-        number: 7,
-        Description: "Expertise in the field, consistently exceeding expectations.",
-        LevelUnlockScore: 500,
-    },
-    {
-        Name: "The Masterful Professional",
-        number: 8,
-        Description: "Mastering the craft, exceptional performance.",
-        LevelUnlockScore: 1500,
-    },
-    {
-        Name: "The Leadership Prodigy",
-        number: 9,
-        Description: "Displaying leadership qualities, making a significant impact.",
-        LevelUnlockScore: 3000,
-    },
-    {
-        Name: "The Boss",
-        number: 10,
-        Description: "A true leader and expert, sets the standard for excellence.",
-        LevelUnlockScore: 10000,
-    },
-];
+// type Level = {
+//   Name: string
+//   number: number
+//   Description: string
+//   LevelUnlockScore: number
+// }
 let users = [];
 const retrieveUsers = localStorage.getItem("users");
 if (retrieveUsers) {
--- quicklist.view.ts
+++ quicklist.view.ts
@@ -1,40 +1,76 @@
-import { difficulties, projects } from "./quicklist.model.js";
+import { difficulties, projects, Project } from "./quicklist.model.js"
 
 export function displayProjectDifficulties() {
-    const container = document.getElementById("projectDifficulty") as HTMLSelectElement;
-if (container) {
-    container.innerHTML = `${difficulties.map(toOption).join("\n")}`;
-}
+  const container = document.getElementById(
+    "projectDifficulty",
+  ) as HTMLSelectElement
+  if (container) {
+    container.innerHTML = `${difficulties.map(toOption).join("\n")}`
+  }
 }
 
 function toOption(difficulty: string) {
-    return `<option value=${difficulty}>${difficulty}</option>`
+  return `<option value=${difficulty}>${difficulty}</option>`
 }
 
 function getProjectInfo(id: string) {
-    const chosenProject = projects.find(project => project.id === id);
+  const chosenProject = projects.find((project) => project.id === id)
 
-    if (!chosenProject) {
-        throw new Error(`Project wasn't found!`);
-    }
+  if (!chosenProject) {
+    throw new Error(`Project wasn't found!`)
+  }
 
-    return chosenProject;
+  return chosenProject
 }
 
 export function loadProjectInfo(id: string) {
-    const chosenProject = getProjectInfo(id);
+  const chosenProject = getProjectInfo(id)
 
-    document.getElementById("projectInfoOwner")!.innerText = `${chosenProject.owner}`
-    document.getElementById("projectInfoDeadline")!.innerText = `${parseDate(chosenProject.deadline)}`
-    document.getElementById("projectInfoDifficulty")!.innerText = `${chosenProject.difficulty}`
-    document.getElementById("projectInfoBudget")!.innerText = `${chosenProject.budget}`
-    document.getElementById("projectInfoStatus")!.innerText = `${chosenProject.status}`
-    document.getElementById("projectInfoDetails")!.innerText = `${chosenProject.details}`
-
-
+  document.getElementById(
+    "projectInfoOwner",
+  )!.innerText = `${chosenProject.owner}`
+  document.getElementById("projectInfoDeadline")!.innerText = `${parseDate(
+    chosenProject.deadline,
+  )}`
+  document.getElementById(
+    "projectInfoDifficulty",
+  )!.innerText = `${chosenProject.difficulty}`
+  document.getElementById(
+    "projectInfoBudget",
+  )!.innerText = `${chosenProject.budget}`
+  document.getElementById(
+    "projectInfoStatus",
+  )!.innerText = `${chosenProject.status}`
+  document.getElementById(
+    "projectInfoDetails",
+  )!.innerText = `${chosenProject.details}`
 }
 
 function parseDate(date: Date) {
-    const dateString = date.toString();
-    return dateString.replace('T', ' ');
+  const dateString = date.toString()
+  return dateString.replace("T", "  ")
+}
+
+export function appendHTML() {
+  const projectsContainer = document.querySelector(
+    ".your-projects__container__list",
+  )
+  if (projectsContainer) {
+    projects.forEach((project) => {
+      const projectHTML = generateProjectHTML(project)
+      projectsContainer.innerHTML += projectHTML
+    })
+  }
+}
+
+function generateProjectHTML(project: Project): string {
+  return `
+    <li class="your-projects__container__list-item">
+      <a id="${project.id}" href="#" class="__project-title">${
+    project.title
+  }</a>
+      <p class="__project-owner">${project.owner}</p>
+      <p class="__project-deadline">${parseDate(project.deadline)}</p>
+    </li>
+  `
 }
