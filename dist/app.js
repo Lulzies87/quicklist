4294302d83cc72b09f8b2ad337e374cdd7da8cc4
Assaf Basri <Faskoman90@gmail.com>
2023-11-30 18:50:31 +0200

    feat(appent)

--- dist/app.js
+++ dist/app.js
@@ -1,9 +1,10 @@
 import { loadPage } from "./quicklist.controller.js";
 import { loadProjectInfo, appendHTML } from "./quicklist.view.js";
+const currentUser = JSON.parse(sessionStorage.getItem("currentUser"));
 loadPage();
 if (window.location.pathname === '/project_details.html') {
     loadProjectInfo("f30f7f36-f9ad-47e4-b43d-c1fad7cb1c84"); // TODO: find a way to get the project id when clicking on a project in the dashboard
 }
 if (window.location.pathname === '/dashboard.html') {
-    appendHTML();
+    appendHTML(currentUser);
 }
