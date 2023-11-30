b4046142d102d7e80c510ec5ba8eb383bfd4c3db
Assaf Basri <Faskoman90@gmail.com>
2023-11-30 09:51:16 +0200

    pushed

--- login.ts
+++ login.ts
@@ -67,7 +67,7 @@ addUserForm?.addEventListener("submit", function (e) {
 
     window.localStorage.setItem("users", JSON.stringify(users))
     sessionStorage.setItem("user", username)
-    window.location.href = "index.html"
+    window.location.href = "dashboard.html"
   }
 })
 
